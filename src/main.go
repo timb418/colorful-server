ad32423c057c4a8a1beab28cb7fbada11274f423
Timur <10380147+ShadyBytes@users.noreply.github.com>
2022-10-07 16:29:34 +0300

    cleanup

--- src/main.go
+++ src/main.go
@@ -8,19 +8,12 @@ import (
 )
 
 func main() {
-	//db := getDbConnection()
-	//initDb(db)
-
 	http.HandleFunc("/colors", getColors)
 	err := http.ListenAndServe(":8090", nil)
 	if err != nil {
 		return
 	}
 
-	//err = db.Close()
-	//if err != nil {
-	//	return
-	//}
 }
 
 func getColors(writer http.ResponseWriter, request *http.Request) {
@@ -28,11 +21,9 @@ func getColors(writer http.ResponseWriter, request *http.Request) {
 	rgb := request.URL.Query().Get("rgb")
 	colors := initColorsNamesSet()
 	fmt.Println(colors)
-	//db, err := sql.Open("sqlite3", "colors.db")
-	//db := getDbConnection()
-	//initDb(db)
+
 	colorName := getColorNameByRgb(rgb, colors)
-	//page := Page{PageTitle: "test page title", ColorRGB: template.CSS(rgb)}
+
 	page := Page{PageTitle: colorName, ColorRGB: template.CSS(rgb)}
 	err := tmpl.Execute(writer, page)
 	if err != nil {
